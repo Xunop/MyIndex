bc9d50c655154b4f56db0a9540ac71e954995db8
Xunop <1919581623@qq.com>
2022-08-18 17:10:09 +0800

    fix：修改重定向

--- one/vh_resource/main.js
+++ one/vh_resource/main.js
@@ -459,7 +459,7 @@ function caliButton(TakiIn) {
         imgStatue2: 'qicao_stand2',
         linkToName: "穿梭",
       });
-      linkToSrc = "../one";
+      linkToSrc = "/one.html";
       break;
     case 'music':
       cleanChrImg();
